a9a03379a50dd258d0ba27713fc63e45b2551b75
michael <mikhaelojox@gmail.com>
2021-06-12 13:32:07 +0100

    commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramojodev",
+    "password": "udacitymic7",
+    "database": "udagramojodb",
+    "host": "udagramojodbdev.ceq3jjqxkxy4.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udacitymics3-cloud-dev"
   },
   "prod": {
     "username": "",
